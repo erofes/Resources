611e63ea2654c5358f7ef61d5e18bba90b3fa618
erofes <erofes@mail.ru>
2017-08-19 13:02:16 +0700

    Start

---
+++ Miiico.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miiico : MonoBehaviour {
+    public Rigidbody m_rb;
+	// Use this for initialization
+	void Start () {
+        m_rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_rb.AddTorque(0.1f, 0.1f, 0.1f, ForceMode.Acceleration);
+    }
+}
