2f8e67638266bdf42fd353112b8c4296de647aeb
erofes <erofes@mail.ru>
2017-08-19 12:21:42 +0700

    3ewqewq

--- 55555555555555555.cs
+++ 55555555555555555.cs
@@ -6,8 +6,10 @@ public class 55555555555555555 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// Jopa Jopa Jopa
-	}
+    // Jopa Jopa Jopa
+
+// TRY IT PLACE HOLDER
+}
 	
 	// Update is called once per frame
 	void Update () {
